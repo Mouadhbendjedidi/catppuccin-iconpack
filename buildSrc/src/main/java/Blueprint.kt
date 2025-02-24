27110704e5157eccf1e78873d5f882f42d8314cf
Mouadhbendjedidi <alfadjr2007@gmail.com>
2025-02-24 20:22:24 +0100

    setup the iconpack dashboard essentials

---
+++ buildSrc/src/main/java/Blueprint.kt
@@ -0,0 +1,7 @@
+@file:Suppress("unused")
+
+object Blueprint {
+    const val appId = "dev.jahir.blueprint.app"
+    const val version = 241
+    const val versionName = "2.4.1"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,23 @@
+@file:Suppress("unused")
+
+object Versions {
+    // Plugins
+    const val gradle = "8.3.0"
+    const val kotlin = "1.9.23"
+    const val sonatype = "1.3.0"
+    const val ksp = "$kotlin-1.0.20"
+
+    // OneSignal
+    const val oneSignal = "4.8.8"
+
+    // App
+    const val minSdk = 21
+    const val targetSdk = 34
+    const val buildTools = "34.0.0"
+
+    // Kuper
+    const val kuper = "2.5.1"
+
+    // Adaptive Icons
+    const val adaptiveIcons = "1.0.1"
+}
